5b3bd6a6ae0280ab31b3d9b355989356b53abd1f
ryancirish <ryancirish@users.noreply.github.com>
2023-07-08 17:07:40 -0400

    adding db

--- api/index.py
+++ api/index.py
@@ -1,6 +1,18 @@
 from sanic import Sanic
 from sanic.response import json
-app = Sanic()
+from tortoise.contrib.sanic import register_tortoise
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_url = os.getenv('DB_URL')
+
+app = Sanic(__name__)
+
+register_tortoise(
+    app, db_url=db_url, modules={"models": ["models"]}, generate_schemas=True
+)
  
  
 @app.route('/')
---
+++ api/models.py
@@ -0,0 +1,6 @@
+from tortoise import Model, fields
+
+class Emails(model):
+    id = fields.IntField(pk=True)
+    created_at = fields.DatetimeField(null=True, auto_now_add=True)
+    email = fields.CharField(320, unique=True)
